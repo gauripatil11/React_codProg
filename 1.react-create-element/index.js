d0e870df2c13720c09eebaf1925e4028b19f1b7c
Gauri Patil <gaurip331@gmail.com>
2024-01-22 12:01:03 +0530

    create react app without using ant tool

---
+++ 1.react-create-element/index.js
@@ -0,0 +1,14 @@
+// 1>>>>
+// const el = React.createElement('h1',null,'gauriiii')
+// ReactDOM.createRoot(document.querySelector('#root')).render(el)
+
+
+// 2>>>>
+// ReactDOM.createRoot(document.querySelector('#root')).render(<h1>gauriiiiiiiiii</h1>)
+
+
+// 3>>>>
+// function Hello(){
+//     return <h1>gauriiiii</h1>
+// }
+// ReactDOM.createRoot(document.querySelector('#root')).render(<Hello/>)
